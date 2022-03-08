55cd620b54687ac2ea1824620512aee4138330c6
Cristobal Hermida Portales <cristobalhp@ext.zara.com>
2022-03-08 11:04:40 +0100

    add classes for Voucher pricing rule, implementing applyDiscount method

---
+++ src/main/java/pricing/VoucherPricingRuleOne.java
@@ -0,0 +1,25 @@
+package pricing;
+
+public class VoucherPricingRuleOne implements PricingRule {
+
+  private static final double PRICE_DISCOUNT = 5.00;
+
+  private static final Integer ITEMS_AMOUNT = 3;
+
+  @Override
+  public double applyDiscount(double productPrice, Long itemAmount) {
+    double price;
+    if (itemAmount < ITEMS_AMOUNT) {
+      price = productPrice;
+    } else {
+      var resto = itemAmount % 2;
+      var count = itemAmount / 2;
+      if (resto == 0) {
+        price = count * PRICE_DISCOUNT;
+      } else {
+        price = count * PRICE_DISCOUNT + productPrice;
+      }
+    }
+    return price;
+  }
+}
