a72259085dfe2dea4a84b62b35001b48c828f6fc
Cristobal Hermida Portales <cristobalhp@ext.zara.com>
2022-03-08 13:53:14 +0100

    refactor updating test for testing CashRegister and PricingRules

--- src/test/java/CashRegisterTest.java
+++ src/test/java/CashRegisterTest.java
@@ -1,9 +1,18 @@
 import checkout.CheckOut;
 import checkout.CheckOutStoreOne;
 import checkout.CheckOutStoreTwo;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import pricing.PantsPricingRule;
+import pricing.PantsPricingRuleTwo;
+import pricing.PricingRule;
+import pricing.TshirtPricingRule;
+import pricing.VoucherPricingRuleOne;
+import pricing.VoucherPricingRuleTwo;
 import product.Product;
+import product.ProductType;
 
 class CashRegisterTest {
 
@@ -11,7 +20,7 @@ class CashRegisterTest {
   void testNoApplyDiscountScenario() {
 
     //VOUCHER, TSHIRT, PANTS - Total: 32.50
-    CheckOut checkOut = new CheckOutStoreOne();
+    CheckOut checkOut = new CheckOutStoreOne(getPricingRulesScenarioOne());
     CashRegister cashier = new CashRegister(checkOut);
     cashier.addItem(new Product("VOUCHER", "Gift Card", 5.00));
     cashier.addItem(new Product("PANTS", "Summer Pants", 7.50));
@@ -26,7 +35,7 @@ class CashRegisterTest {
   void testApplyDiscountScenario2() {
 
     // VOUCHER, TSHIRT, VOUCHER - Total: 25.00€
-    CheckOut checkOut = new CheckOutStoreOne();
+    CheckOut checkOut = new CheckOutStoreOne(getPricingRulesScenarioOne());
     CashRegister cashier = new CashRegister(checkOut);
     cashier.addItem(new Product("VOUCHER", "Gift Card", 5.00));
     cashier.addItem(new Product("VOUCHER", "Gift Card", 5.00));
@@ -41,7 +50,7 @@ class CashRegisterTest {
   void testApplyDiscountScenario3() {
 
     // TSHIRT, TSHIRT, TSHIRT, VOUCHER, TSHIRT - Total: 81.00€
-    CheckOut checkOut = new CheckOutStoreOne();
+    CheckOut checkOut = new CheckOutStoreOne(getPricingRulesScenarioOne());
     CashRegister cashRegister = new CashRegister(checkOut);
     cashRegister.addItem(new Product("TSHIRT", "Summer T-Shirt", 20.00));
     cashRegister.addItem(new Product("VOUCHER", "Gift Card", 5.00));
@@ -58,7 +67,7 @@ class CashRegisterTest {
   void testApplyDiscountScenario4() {
 
     //   VOUCHER, TSHIRT, VOUCHER, VOUCHER, PANTS, TSHIRT, TSHIRT - Total: 74.50€
-    CheckOut checkOut = new CheckOutStoreOne();
+    CheckOut checkOut = new CheckOutStoreOne(getPricingRulesScenarioOne());
     CashRegister cashRegister = new CashRegister(checkOut);
     cashRegister.addItem(new Product("PANTS", "Summer Pants", 7.50));
     cashRegister.addItem(new Product("VOUCHER", "Gift Card", 5.00));
@@ -77,7 +86,7 @@ class CashRegisterTest {
   void testApplyDiscountOnCheckOutStoreTwo() {
 
     //   VOUCHER, TSHIRT, VOUCHER, VOUCHER, PANTS,PANTS,PANTS,TSHIRT, TSHIRT - Total: (90)€
-    CheckOut checkOut = new CheckOutStoreTwo();
+    CheckOut checkOut = new CheckOutStoreTwo(getPricingRulesScenarioTwo());
     CashRegister cashRegister = new CashRegister(checkOut);
     cashRegister.addItem(new Product("PANTS", "Summer Pants", 7.50));
     cashRegister.addItem(new Product("PANTS", "Summer Pants", 7.50));
@@ -91,6 +100,22 @@ class CashRegisterTest {
     var result = cashRegister.getTotalAmount();
     Assertions.assertEquals(90.00, result);
 
+  }
+
+  private List<PricingRule> getPricingRulesScenarioOne(){
+    PricingRule pricingVoucherRule = new VoucherPricingRuleOne(ProductType.VOUCHER);
+    PricingRule pricingPantsRule = new PantsPricingRule(ProductType.PANTS);
+    PricingRule pricingTshirtRulerRule = new TshirtPricingRule(ProductType.TSHIRT);
+
+    return List.of(pricingVoucherRule,pricingPantsRule,pricingTshirtRulerRule);
+  }
+
+  private List<PricingRule> getPricingRulesScenarioTwo(){
+    PricingRule pricingVoucherRule = new VoucherPricingRuleTwo(ProductType.VOUCHER);
+    PricingRule pricingPantsRule = new PantsPricingRuleTwo(ProductType.PANTS);
+    PricingRule pricingTshirtRulerRule = new TshirtPricingRule(ProductType.TSHIRT);
+
+    return List.of(pricingVoucherRule,pricingPantsRule,pricingTshirtRulerRule);
   }
 
 }
--- src/test/java/PricingRuleTest.java
+++ src/test/java/PricingRuleTest.java
@@ -2,15 +2,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import checkout.CheckOutStoreOne;
 import checkout.CheckOutStoreTwo;
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import pricing.PantsPricingRule;
+import pricing.PantsPricingRuleTwo;
+import pricing.PricingRule;
+import pricing.TshirtPricingRule;
+import pricing.VoucherPricingRuleOne;
+import pricing.VoucherPricingRuleTwo;
 import product.Product;
+import product.ProductType;
 
 class PricingRuleTest {
 
   @Test
   void testApplyDiscountTshirt() {
 
-    var checkOut = new CheckOutStoreOne();
+    var checkOut = new CheckOutStoreOne(getPricingRulesScenarioOne());
     var item = new Product("TSHIRT", "Summer T-Shirt", 20.00);
     var pricingRule = checkOut.scan(item);
     var result = pricingRule.applyDiscount(item.getPrice(), 3L);
@@ -21,7 +29,7 @@ class PricingRuleTest {
   @Test
   void testNoApplyDiscountSuccessTshirt() {
 
-    var checkOut = new CheckOutStoreOne();
+    var checkOut = new CheckOutStoreOne(getPricingRulesScenarioOne());
     var item = new Product("TSHIRT", "Summer T-Shirt", 20.00);
     var pricingRule = checkOut.scan(item);
     var result = pricingRule.applyDiscount(item.getPrice(), 2L);
@@ -31,7 +39,7 @@ class PricingRuleTest {
 
   @Test
   void testApplyDiscountVoucher() {
-    var checkOut = new CheckOutStoreOne();
+    var checkOut = new CheckOutStoreOne(getPricingRulesScenarioOne());
     var item = new Product("VOUCHER", "Gift Card", 5.00);
     var pricingRule = checkOut.scan(item);
     var result = pricingRule.applyDiscount(item.getPrice(), 3L);
@@ -41,7 +49,7 @@ class PricingRuleTest {
 
   @Test
   void testNoApplyDiscountSuccessVoucher() {
-    var checkOut = new CheckOutStoreOne();
+    var checkOut = new CheckOutStoreOne(getPricingRulesScenarioOne());
     var item = new Product("VOUCHER", "Gift Card", 5.00);
     var pricingRule = checkOut.scan(item);
     var result = pricingRule.applyDiscount(item.getPrice(), 2L);
@@ -51,7 +59,7 @@ class PricingRuleTest {
 
   @Test
   void testApplyDiscountWith4Voucher() {
-    var checkOut = new CheckOutStoreOne();
+    var checkOut = new CheckOutStoreOne(getPricingRulesScenarioOne());
     var item = new Product("VOUCHER", "Gift Card", 5.00);
     var pricingRule = checkOut.scan(item);
     var result = pricingRule.applyDiscount(item.getPrice(), 4L);
@@ -60,8 +68,8 @@ class PricingRuleTest {
   }
 
   @Test
-  void testApplyNoDiscountWithPants() {
-    var checkOut = new CheckOutStoreOne();
+  void testApplyNoDiscountWithPantsCheckOutOne() {
+    var checkOut = new CheckOutStoreOne(getPricingRulesScenarioOne());
     var item = new Product("PANTS", "Summer Pants", 7.50);
     var pricingRule = checkOut.scan(item);
     var result = pricingRule.applyDiscount(item.getPrice(), 2L);
@@ -70,8 +78,8 @@ class PricingRuleTest {
   }
 
   @Test
-  void testApplyDiscountWithPants() {
-    var checkOut = new CheckOutStoreTwo();
+  void testApplyDiscountWithPantsCheckOutTwo() {
+    var checkOut = new CheckOutStoreTwo(getPricingRulesScenarioTwo());
     var item = new Product("PANTS", "Summer Pants", 7.50);
     var pricingRule = checkOut.scan(item);
     var result = pricingRule.applyDiscount(item.getPrice(), 3L);
@@ -81,7 +89,7 @@ class PricingRuleTest {
 
   @Test
   void testApplyDiscountWithEmptyProduct() {
-    var checkOut = new CheckOutStoreTwo();
+    var checkOut = new CheckOutStoreTwo(getPricingRulesScenarioOne());
     var item = new Product("", "", 0d);
     var pricingRule = checkOut.scan(item);
     var result = pricingRule.applyDiscount(item.getPrice(), 1L);
@@ -90,10 +98,26 @@ class PricingRuleTest {
 
   @Test
   void testApplyDiscountWithNoExistingDiscountForProduct() {
-    var checkOut = new CheckOutStoreTwo();
+    var checkOut = new CheckOutStoreTwo(getPricingRulesScenarioOne());
     var item = new Product("COAT", "winter coat", 40.0);
     var pricingRule = checkOut.scan(item);
     var result = pricingRule.applyDiscount(item.getPrice(), 1L);
     assertEquals(40, result);
+  }
+
+  private List<PricingRule> getPricingRulesScenarioOne(){
+    PricingRule pricingVoucherRule = new VoucherPricingRuleOne(ProductType.VOUCHER);
+    PricingRule pricingPantsRule = new PantsPricingRule(ProductType.PANTS);
+    PricingRule pricingTshirtRulerRule = new TshirtPricingRule(ProductType.TSHIRT);
+
+    return List.of(pricingVoucherRule,pricingPantsRule,pricingTshirtRulerRule);
+  }
+
+  private List<PricingRule> getPricingRulesScenarioTwo(){
+    PricingRule pricingVoucherRule = new VoucherPricingRuleTwo(ProductType.VOUCHER);
+    PricingRule pricingPantsRule = new PantsPricingRuleTwo(ProductType.PANTS);
+    PricingRule pricingTshirtRulerRule = new TshirtPricingRule(ProductType.TSHIRT);
+
+    return List.of(pricingVoucherRule,pricingPantsRule,pricingTshirtRulerRule);
   }
 }
