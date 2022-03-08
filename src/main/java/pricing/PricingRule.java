8f39ab28416ef927ba974d99e7791368e0db7004
Cristobal Hermida Portales <cristobalhp@ext.zara.com>
2022-03-08 13:51:42 +0100

    coverted PricingRule to abstract class with consutrtor product type pricing discount

--- src/main/java/pricing/PricingRule.java
+++ src/main/java/pricing/PricingRule.java
@@ -1,5 +1,18 @@
 package pricing;
 
-public interface PricingRule {
-  double applyDiscount(double price,Long itemAmount);
+import product.ProductType;
+
+public abstract class PricingRule {
+
+  private ProductType type;
+
+  protected PricingRule(ProductType type) {
+    this.type = type;
+  }
+
+  public abstract double applyDiscount(double price, Long itemAmount);
+
+  public ProductType getType() {
+    return type;
+  }
 }
